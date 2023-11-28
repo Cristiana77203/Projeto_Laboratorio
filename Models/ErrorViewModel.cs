e04f53ddce59decb18fc5ac3e35e9ddd37efb7d1
Cristiana77203 <cristianabarbosa2000@gmail.com>
2023-11-28 19:07:22 +0000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projeto_Laboratorio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
